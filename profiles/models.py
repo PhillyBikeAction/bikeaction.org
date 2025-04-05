PhillyBikeAction/abp

initial sketch of an organizer specific django admin
This allows us to configure a set of organizers per-district.

Once configured, these users will have access to a limited Django admin that filters records by the districts relevant to that orgaizer.

profiles/models.py
@@ -171,5 +171,9 @@ class Profile(models.Model):
             .all()
         )
 
+    @property
+    def is_organizer(self):
+        return bool(self.user.profile.organizers.all())
+
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.user.email}"

PhillyBikeAction/bikeaction.org

profiles/models.py
@@ -182,6 +182,10 @@ class Profile(models.Model):
             .all()
         )
 
+    @property
+    def is_organizer(self):
+        return bool(self.user.profile.organized_districts.all())
+
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.user.email}"
 