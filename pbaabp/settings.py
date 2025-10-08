6f39c65089cc0b00011275ffcdfdd24cd8ed582a
Ee Durbin <ewdurbin@gmail.com>
2025-10-08 09:02:50 -0400

    setup a runner for lint/tests in github actions

--- pbaabp/settings.py
+++ pbaabp/settings.py
@@ -181,6 +181,11 @@ CONN_HEALTH_CHECKS = env.bool("DJANGO_CONN_HEALTH_CHECKS", default=True)
 if CONN_HEALTH_CHECKS:
     DATABASES["default"]["CONN_HEALTH_CHECKS"] = CONN_HEALTH_CHECKS
 
+# Speed up tests by skipping migrations and creating schema directly from models
+DATABASES["default"]["TEST"] = {
+    "MIGRATE": False,
+}
+
 _REDIS_URL = env("REDIS_URL", default="redis://redis:6379/0")
 if _REDIS_URL.startswith("rediss://"):
     _REDIS_URL = _REDIS_URL + "?ssl_cert_reqs=none"
